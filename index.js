490368c79d5b96c99f41705c32cd177e9937e2d7
Sachin Verma <sachinverma53121@gmail.com>
2021-02-26 10:13:57 +0530

    fetch paralley

--- index.js
+++ index.js
@@ -22,29 +22,46 @@ app.get("/api", async (req, res) => {
   mandiData = [];
   const mainURL = "https://www.commodityonline.com/mandiprices/";
   const urls = ["https://www.commodityonline.com/mandiprices/"];
-  for (let i = 1; i <= 9; ++i) {
-    urls.push(`${mainURL}${36 * i}`);
+
+  let axios_requests = [];
+  axios_requests.push(axios.get(`${mainURL}`));
+
+  for (let i = 1; i <= 10; ++i) {
+    axios_requests.push(axios.get(`${mainURL}${36 * i}`));
   }
 
-  for (let url of urls) {
-    console.log("scraped data from: " + url);
-    const response = await axios.get(url);
-    let $ = cheerio.load(response.data);
+  Promise.all([...axios_requests])
+    .then(function (results) {
+      for (let i = 0; i < results.length; ++i) {
+        console.log("hitted api: ", i);
 
-    $("#tdm_base_scroll > div > div.dt_ta_09").each(function (i, elm) {
-      let price = $("div.dt_ta_14", elm);
-      let newObject = {
-        commodity: $("div.dt_ta_10", elm).text().trim(),
-        marketCenter: $("div.dt_ta_11", elm).text().trim().replace("\n", ","),
-        variety: $("div.dt_ta_12", elm).text().trim(),
-        arrrivals: $("div.dt_ta_13", elm).text().trim(),
-        modalPrice: $(price[0]).text().trim(),
-        minMaxPrice: $(price[1]).text().trim().replace("\n", " "),
-      };
-      mandiData.push(newObject);
+        const result = results[i].data;
+        let $ = cheerio.load(result);
+
+        $("#tdm_base_scroll > div > div.dt_ta_09").each(function (i, elm) {
+          let price = $("div.dt_ta_14", elm);
+          let newObject = {
+            commodity: $("div.dt_ta_10", elm).text().trim(),
+            marketCenter: $("div.dt_ta_11", elm)
+              .text()
+              .trim()
+              .replace("\n", ","),
+            variety: $("div.dt_ta_12", elm).text().trim(),
+            arrrivals: $("div.dt_ta_13", elm).text().trim(),
+            modalPrice: $(price[0]).text().trim(),
+            minMaxPrice: $(price[1]).text().trim().replace("\n", " "),
+          };
+          mandiData.push(newObject);
+        });
+      }
+
+      console.log("end");
+      res.status(200).send(mandiData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send("error fetching");
     });
-  }
-  res.status(200).send(mandiData);
 });
 
 app.get("*", (req, res) => res.status(400).send("Wrong route use /api"));
